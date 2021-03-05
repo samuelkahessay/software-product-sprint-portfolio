0237745b8694e335106c4116de267234dd89dc2e
Samuel Kahessay <samuelkahessay@gmail.com>
2021-03-05 21:09:33 +0000

    Add basic hello world message to servlet  at backslash hello

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -13,6 +13,8 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>hello world!</h1>");
+    response.getWriter().println("<h1>this message is coming from the java servlet and can be accessed at \"&#47; hello\" </h1>");
+
   }
 }
