4b11f72b3ea49d806fb5fda547a7596c43cc4e18
Samuel Kahessay <samuelkahessay@gmail.com>
2021-03-06 00:24:03 +0000

    Complete form handler that redirects to Pitchfork with user inputted album

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,31 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.*;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the value entered in the form.
+    String textValue = request.getParameter("text-input");
+
+    // Print the value so you can see it in the server logs.
+    System.out.println("You submitted: " + textValue);
+
+    // Write the value to the response so the user can see it.
+    response.getWriter().println("Your favourite album is: " + textValue);
+
+    // Parse response so spaces are seen as "%20"
+    response.sendRedirect("https://pitchfork.com/search/?query=" + URLEncoder.encode(textValue, StandardCharsets.UTF_8));
+
+  }
+}
